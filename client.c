3a3ef237892272d6ce9616ecf380e041471cfc21
Marina Alba Rodriguez Estebanez <117929211+MofMiq@users.noreply.github.com>
2023-03-27 16:56:36 +0200

    ok - comments without correction

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: marirodr <marirodr@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:12:48 by marirodr          #+#    #+#             */
-/*   Updated: 2023/03/22 16:28:23 by marirodr         ###   ########.fr       */
+/*   Updated: 2023/03/27 15:35:13 by marirodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 /*
 kill: enviar.
-usleep: crear un delay  para que el caracter en binario no se lie y salga 
-ordenado.
-"imprime" el byte t lo envia. si es un 1 envia  SIGUSR1, si es 0, envia SIGUSR2
+usleep: crear un delay que permite que el receptor tenga tiempo suficiente para
+procesar la señal.
+"descompone" el byte y lo envia al server. se definen las señales SIGUSR1 y
+SIGUSR2:si es un 1 envia  SIGUSR1, si es 0, envia SIGUSR2
 */
 
 void	ft_send_msg(int pid, char byte)
@@ -27,34 +28,48 @@ void	ft_send_msg(int pid, char byte)
 	while (i < 8)
 	{
 		if ((byte & (1 << i)) != 0)
-		{
-			kill(pid, SIGUSR1);		
-		}
+			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(50);
+		usleep(40);
 		i++;
 	}
 }
 
+/*esta funcion se utiliza como manejador de señales. si recibe uan señal SIGUSR1
+muestra un mensaje indicando  que el mensaje ha sido recibido correctamente por
+el server.*/
+
 void	ft_handler(int a)
 {
 	if (a == SIGUSR1)
-		ft_printf("The message has been received successfully.");
+		ft_printf("%sThe message has been received successfully.%s\n", GREEN,
+			END);
 }
+
+/*el programa recibe dos argumentos en la linea de comando: el pid y un texto.
+cconvertimos el pid en un entero con ft_atoi. ademas, se crea una estructura
+'sigaction' (msg) que apunta al manejador de señales ft_handler y se inicializa
+la estructura 'sig' para configurar el comportamiento de la señal SIGUSR1 
+utilizando al funcion 'sigaction' -en este caso, para que cuando reciba del
+server la señal SIGUSR1, que significa que el mensaje ha terminado de recibirse
+, que ejecute la funcion 'ft_handler'. luego se envia
+cada caracter del texto utilizando la funcion 'ft_send_msg'. Cuando todos los
+caracteres del mensaje se han enviado, se envia un caracter nulo para indicar
+el final del mismo.*/
 
 int	main(int argc, char **argv)
 {
 	int					pid;
 	int					i;
-	struct sigaction	sig;
+	struct sigaction	msg;
 
 	i = 0;
 	if (argc == 3)
 	{
 		pid = ft_atoi(argv[1]);
-		sig.sa_handler = &ft_handler;
-		sigaction(SIGUSR1, &sig, NULL);
+		msg.sa_handler = &ft_handler;
+		sigaction(SIGUSR1, &msg, NULL);
 		while (argv[2][i] != '\0')
 		{
 			ft_send_msg(pid, argv[2][i]);
@@ -63,6 +78,6 @@ int	main(int argc, char **argv)
 		ft_send_msg(pid, '\0');
 	}
 	else
-		ft_printf("Format error: <PID> <text>\n");
+		ft_printf("%sFormat error: <PID> <text>%s\n", RED, END);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,26 +6,11 @@
 /*   By: marirodr <marirodr@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:12:53 by marirodr          #+#    #+#             */
-/*   Updated: 2023/03/22 16:30:38 by marirodr         ###   ########.fr       */
+/*   Updated: 2023/03/27 15:32:11 by marirodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-/*esta funcion es la que recibe la señal del cliente. se encarga de recibir el 
-mensaje y reconstruir los bytes recibidos (decodificar) para mostarrlos en la
-salida estandar.
-
-la funcion se utiliza caracter a caracter (byte a byte)
-
-en el momento en el que el bit esta completo - ya está el char entero en binario-
-hacemos dos comprobaciones: si el byte impreso es el ultimo de la cadena (mesaje)
-imprimimos un salto de linea manualmente y enviamos al SO/otro proceso?? el 1
-con SIGURSR1 con el pid que conseguimos desde la estuctura siginfo_t -> si_pid.
-bit = rastrea la posicion del bit actual en el byte recibido
-byte = byte actual que se esta decodificando, empieza en 0 siempre que estas 
-decodificando el byte, por eso el operador OR para ir sacando los 1s en sus 
-posiciones.*/
 
 void	display_banner(int pid)
 {
@@ -47,6 +32,24 @@ void	display_banner(int pid)
 			\n\n");
 }
 
+/*esta funcion es la que recibe la señal del cliente. se encarga de procesar 
+señales mensaje y reconstruir los bytes recibidos (decodificar) para mostrarlos
+en la salida estandar.
+
+la funcion se utiliza caracter a caracter (byte a byte)
+
+en el momento en el que el bit esta completo - ya está el char entero en binario-
+hacemos dos comprobaciones: si el byte impreso es el ultimo de la cadena (mesaje)
+imprimimos un salto de linea manualmente y enviamos al SO/otro proceso?? el 1
+con SIGURSR1 con el pid que conseguimos desde la estuctura siginfo_t -> si_pid.
+bit = rastrea la posicion del bit actual en el byte recibido
+byte = byte actual que se esta decodificando, empieza en 0 siempre que estas 
+decodificando el byte, por eso el operador OR para ir sacando los 1s en sus 
+posiciones.
+ -> kill(info->si_pid, SIGUSR1); //una vez el mensaje finaliza, enviamos una se-
+ñal SIGUSR1 al proceso que envio el mensaje para confirmar que lo hemos recibido.
+*/
+
 void	ft_decode_message(int signal, siginfo_t *info, void *unused)
 {
 	static int				bit;
@@ -56,7 +59,6 @@ void	ft_decode_message(int signal, siginfo_t *info, void *unused)
 	if (signal == SIGUSR1)
 		byte = byte | (1 << bit);
 	bit++;
-	printf("%d", bit);
 	if (bit == 8)
 	{
 		if (byte == '\0')
@@ -65,27 +67,50 @@ void	ft_decode_message(int signal, siginfo_t *info, void *unused)
 			kill(info->si_pid, SIGUSR1);
 		}
 		else
-			ft_printf("%c", byte);
+			ft_printf("%s%c%s", BLUE, byte, END);
 		bit = 0;
 		byte = 0;
 	}
 }
 
+/*este programa es un buvle infinito que espera recibir señales de SO. El
+programa no hace nada hasta que recibe una señal -por eso pause()-.
+En la estructura sigaction, se inicializa la funcion sa_sigaction en la dire-
+ccion de memoria de la funcion decode_message, para que sepa que funcion
+debe llamar cuando se reciba una señal y que ésta la procese.
+La estructura de sigaction se usa para configurar el comportamiento
+de la señal.
+sigemptyset: inicializa el conjunto de señales vacio
+sigaddset: agrega una señal especifica al conjunto de señales
+estas funciones se encargan de configurar correctamente la mascara de señales
+para evitar recibir señales no deseadas que afecten al la ejecucion normal
+del programa.
+struct sigaction msg: esta estructura se usa para configurar el comporta-
+miento de la señal
+msg.sa_sigaction = &ft_decode_message: se establece el miembro 'sa_sigaction'
+en la direccion de memoria de la funcion "ft_decode_message", para indicarle 
+que ésta debe ser llamada cuando reciba una señal
+msg.sa_flags = SA_SIGINFO: se establece la bandera "SA_SIGINFO" para indicar
+que queremos utilizar la estructura "siginfo_t" para obtener informacion sobre
+la señal.
+*/
+
 int	main(void)
 {
-	struct sigaction	sig;
+	struct sigaction	msg;
 
 	display_banner(getpid());
-	sig.sa_sigaction = &ft_decode_message;
-	sig.sa_flags = SA_SIGINFO;
-	//sigemptyset(&sig.sa_mask);
-	//sigaddset(&sig.sa_mask, SIGUSR1);
-	//sigaddset(&sig.sa_mask, SIGUSR2);
+	msg.sa_sigaction = &ft_decode_message;
+	msg.sa_flags = SA_SIGINFO;
 	while (1)
 	{
-		sigaction(SIGUSR1, &sig, NULL);
-		sigaction(SIGUSR2, &sig, NULL);
+		sigaction(SIGUSR1, &msg, NULL);
+		sigaction(SIGUSR2, &msg, NULL);
 		pause();
 	}
 	return (0);
 }
+
+	//sigemptyset(&msg.sa_mask);
+	//sigaddset(&msg.sa_mask, SIGUSR1);
+	//sigaddset(&msg.sa_mask, SIGUSR2);
